beb2e2e9eb302453499cedc9c746207f97a6e6c2
ibro <sameoldlab@proton.me>
2025-02-26 12:02:49 +0100

    cta ui

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+/*
+cannor use server actions on a prerendered page
+import type { Actions } from './$types'
+
+export const actions = {
+  default: async (event) => {
+    console.log(event)
+  }
+} satisfies Actions */
--- src/routes/components/hero/Cta.svelte
+++ src/routes/components/hero/Cta.svelte
@@ -1,13 +1,40 @@
+<script lang="ts">
+  // import { enhance } from "$app/forms";
+  let isExtra = $state(false);
+  let username = $state("");
+  const togglePlan = (e: MouseEvent) => {
+    e.preventDefault();
+    if ((e.currentTarget as HTMLButtonElement).innerText.includes("Extra"))
+      isExtra = true;
+    else isExtra = false;
+  };
+</script>
+
 <div class="cta">
-  <form action="post">
+  <form>
     <div class="cta-plans">
-      <button>Extra weird $25 <span>50% off</span></button>
-      <button>Regular Weird <span>free</span></button>
+      <button onclick={togglePlan} role="option" aria-selected={isExtra}
+        >Extra weird $25 <span>50% off</span></button
+      >
+      <button onclick={togglePlan} role="option" aria-selected={!isExtra}
+        >Regular Weird <span>free</span></button
+      >
     </div>
 
     <div class="cta-register">
-      <input type="text" placeholder="a [name].weird.one" />
-      <button>Register</button>
+      <label for="username">
+        <input
+          id="username"
+          bind:value={username}
+          type="text"
+          required
+          placeholder="a [name].weird.one"
+        />
+      </label>
+      <button>
+        Register
+        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+      </button>
     </div>
   </form>
 
@@ -18,37 +45,50 @@
 
 <style>
   button {
-    background: transparent;
     border: 0;
     min-width: max-content;
-    padding: .75rem 2rem;
+    padding: 0.75rem 2rem;
     min-width: max-content;
+    cursor: pointer;
   }
+  button,
+  input {
+    background: oklch(from var(--purple) 0.2 c h / 0.7);
+    color: white;
+    backdrop-filter: blur(8px);
+    font-family: 'Space Mono', monospace;
+  }
+
   form {
     display: grid;
-    gap: .5rem;
+    gap: 0.5rem;
   }
   .cta-plans {
     display: flex;
-    background: blue;
-    padding: 0.25rem;
     border: 1px solid white;
-    background: #fff4;
+    border-radius: 2px;
     align-items: center;
 
     button {
       display: flex;
+      justify-content: space-between;
+      align-items: baseline;
+      padding-inline-end: 1.5rem;
       gap: 2rem;
-      align-items: center;
+      transition: background-color 80ms ease-in-out;
       span {
         padding: 0.5rem 2rem;
-        background: var(--purple);
         border: 1px solid transparent;
         border-radius: 400px;
         width: fit-content;
       }
       &:hover {
-        background: var(--purple);
+        background: oklch(
+          from var(--purple) calc(l - 0.24) calc(c + 0.02) h / 0.8
+        );
+      }
+      &[aria-selected="true"] {
+        background: oklch(from var(--purple) calc(l - 0.16) calc(c + 0.02) h);
         span {
           border-color: white;
         }
@@ -57,22 +97,39 @@
   }
   .cta-register {
     display: flex;
+    border-radius: 2px;
     gap: 0;
     background: #fff4;
     border: 1px solid white;
-    input {
+    label {
       flex: 1 1 0;
+      border-radius: 0;
+    }
+    input {
+      width: 100%;
       padding: 1.25rem 3rem;
       border: 0;
-      border-radius: 0;
-      background: transparent;
     }
     button {
-      padding-inline: 3rem;
-      background: var(--purple);
+      padding-inline-start: 2rem;
+      padding-inline-end: 1.5rem;
+      background: oklch(from var(--purple) calc(l - 0.16) calc(c + 0.02) h);
       border-left: 1px solid white;
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+      justify-content: space-between;
+      svg {
+        transition: transform 100ms ease-in;
+      }
+    &:hover {
+      svg {
+        transform: translate(4px, 0px);
+      }
+    }
     }
   }
+
   .explainers {
     display: flex;
     justify-content: center;
